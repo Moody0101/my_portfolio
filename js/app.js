7ff1e9d47053887a58d7bd205210e6d3a66b9698
moody123-collab <azmoudh@gmail.com>
2021-07-15 15:20:00 +0100

    some changes to the nav bar only

--- js/app.js
+++ js/app.js
@@ -89,23 +89,3 @@ function lighten() {
         ==> making svg.css animation hover.
     */
 }
-const body = document.getElementsByTagName("body")[0];
-const mode = document.getElementById("rounded");
-const rectan = document.getElementById('rectt');
-mode.addEventListener('click', () => {
-    if (mode.style.fill == "") {
-        mode.style.fill = "white";
-        mode.classList.add('mode');
-        mode.classList.remove('mode2');
-        rectan.classList.toggle("dark");
-        body.classList.toggle("ddark");
-    } else if (mode.style.fill == "white") {
-        mode.style.fill = "";
-        mode.classList.remove('mode');
-        mode.classList.add('mode2');
-        rectan.classList.toggle("dark");
-        body.classList.toggle("ddark");
-    }
-
-
-});
