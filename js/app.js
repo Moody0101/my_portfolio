6e1018c8c6279ccc6a8b5a43d4fdb3f804299c2b
Moody0101 <azmoudh@gmail.com>
2021-10-24 14:58:51 +0100

    added workflow and a scrolling animation

--- js/app.js
+++ js/app.js
@@ -1,51 +1,54 @@
+
 const button = document.getElementById("menuTog");
 const bar = document.getElementById('menu');
 const nav = document.querySelector('#navbar ul');
 const skills = document.querySelectorAll(".designs img");
-const paragrahs = document.querySelectorAll(".innerdiv p");
+const paragraphs = document.querySelectorAll(".innerdiv p");
 const git = document.getElementById("lastIm");
-const containernodes = document.getElementsByTagName("section");
+var containernodes = [...document.getElementsByTagName("section")];
 const loadingdiv = document.getElementsByClassName("container");
 const loading = document.getElementById("loading");
 const about = document.querySelectorAll("#about .text p span");
 const aboutimg = document.querySelectorAll("#about .text img");
+const workflow = document.getElementById("workflowIMG");
 
 for(let i=0; i < containernodes.length; i++) {
     containernodes[i].style.display = "none";
 }
-
-// the nav button
+const containers = [
+...about,
+...containernodes, 
+...aboutimg
+];
+for(let i=0; i < containers.length;i++) {
+    containers[i].setAttribute('data-sal', "slide-down");
+    containers[i].setAttribute('data-sal-delay', "500");
+    containers[i].setAttribute('data-sal-repeat', null);
+    containers[i].setAttribute('data-sal-easing', "ease");      
+}
+var animation = sal({
+    once: false,
+    threshold: 0.3
+})
 button.addEventListener('click', function() {
+
     bar.classList.toggle("hide");
     button.classList.toggle("active");
     if (bar.style.display == "") {
         bar.style.display = "flex";
+        
     } else if (!(bar.style.display == "")) {
         bar.style.display = "";
     }
+
 });
 
-// scroll animation code 
-
-function scroll(Nodes, delay, origin = 'bottom', MOBILE = false) {
-    ScrollReveal().reveal(Nodes, {
-        distance: '10%',
-        origin: origin,
-        opacity: 0,
-        DelayNode: delay,
-        reset: true,
-    });
-}
-
-
-scroll(about, 12000);
-scroll(aboutimg, 12000);
 
 // 3d effect in the hero section using vanilla tilt library
 
 VanillaTilt.init(document.querySelectorAll(".cont"), {
     max: 16,
-    speed: 10,
+    speed: 10
 });
 
 // loading animation
---
+++ js/salWrapper.js
@@ -0,0 +1,11 @@
+/*
+* => wrapper for a scroll reveal library.
+*/
+
+import './frameworks/sal.js/dist/sal.js'
+sal();
+export default class sal {
+	constructor() {
+		this.Nodes = Nodes
+	}
+}
