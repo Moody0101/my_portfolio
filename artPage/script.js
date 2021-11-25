afb54d97e017d88d5e7fa193216b07d8acf38bfe
Moody0101 <azmoudh@gmail.com>
2021-11-25 15:18:34 +0100

    added more of my art

---
+++ artPage/script.js
@@ -0,0 +1,27 @@
+// const button = document.getElementById("menuTog");
+// const bar = document.getElementById('menu');
+// const nav = document.querySelector('#navbar ul');
+// const loading = document.getElementById("loading");
+
+
+// button.addEventListener('click', () => {
+//     document.querySelector("#navbar ul ul").classList.toggle("hide");
+//     bar.classList.toggle("hide");
+//     button.classList.toggle("active");
+//     if (bar.style.display == "") {
+//         bar.style.display = "flex";
+//         } else if (!(bar.style.display == "")) {
+//         bar.style.display = "";
+//     }
+
+// });
+
+
+// // setTimeout(() => {
+// //     loadingdiv[0].style.display = "none";
+// //     loading.style.display = "none";
+// //     for(let i=0; i < containernodes.length; i++) {
+// //     containernodes[i].style.display = "";
+// //     containernodes[i].style.transition = "1s all ease";
+// // }
+// // }, 4000);
